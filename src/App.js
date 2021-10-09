b5d05d9a761e780245d78238872522513be2c23d
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-09 21:23:36 +0200

    Navbar Link added with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Router>
+        <Navbar>
+          <Route path='/' exact/>
+        </Navbar>
+      </Router>
+    </>
   );
 }
 
